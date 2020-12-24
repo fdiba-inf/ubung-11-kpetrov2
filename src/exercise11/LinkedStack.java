5517ef83afa5ed55a2193639a67c8db82a4a50b4
kpetrov2 <konstantin.petrov@fdiba.tu-sofia.bg>
2020-12-24 12:52:53 +0000

    24.12

--- src/exercise11/LinkedStack.java
+++ src/exercise11/LinkedStack.java
@@ -5,16 +5,31 @@ public class LinkedStack {
 
     public boolean isEmpty() {
         // Check if stack has no elements
-        return false;
+        return topNode == null;
     }
 
     public void push(String data) {
         // Push element to stack
+        Node node = new Node(data);
+        if (isEmpty()) {
+          topNode = node;
+        }
+        else {
+          node.setNextNode(topNode);
+          topNode = node;
+        }
     }
 
     public String pop() {
         // Pop element from stack
-        return null;
+        if (isEmpty()) {
+          return null;
+        }
+        else {
+          String data = topNode.getData();
+          topNode = topNode.getNextNode();
+          return data;
+        }
     }
 
     @Override
